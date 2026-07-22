576e91bfa7a37d0843d658623ef70b11c7c63f0c
Vansh Pathak <hydravansh24@gmail.com>
2026-07-23 03:49:02 +0530

    performance section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,11 @@ import Hero from './components/Hero'
 import ProductViewer from './components/ProductViewer'
 import gsap from 'gsap'
 import { ScrollTrigger, SplitText } from 'gsap/all'
+import Features from './components/Features'
+import Performance from './components/Performance'
+import Highlights from './components/Highlights'
+import Footer from './components/Footer'
+import Showcase from './components/Showcase'
 
 gsap.registerPlugin(ScrollTrigger, SplitText); 
 
@@ -13,6 +18,11 @@ const App = () => {
       <NavBar />
       <Hero />
       <ProductViewer />
+      <Showcase />
+      <Performance />
+      <Features />
+      <Highlights />
+      <Footer />
     </main>
   )
 }
