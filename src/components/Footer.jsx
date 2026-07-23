39dacc83ec9c7343093f5a344c445a73c7f8a21d
Vansh Pathak <hydravansh24@gmail.com>
2026-07-23 05:45:04 +0530

    footer implemented and the site has been completed and ready to be deployed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import { footerLinks } from '../constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <div className='info'>
+        <p>More ways to shop: Find an Apple Store or other retailer near you. Or call 000000 000 0000.</p>
+        <img src='/logo.svg' alt='Apple Logo' />
+      </div>
+
+      <hr />
+
+      <div className='links'>
+        <p>Copyright © 2024 Apple Inc. All rights reserved.</p>
+
+        <ul>
+          {footerLinks.map(({label, link}) => (
+            <li key={label}>
+              <a href={link}>{label}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
   )
 }
 
